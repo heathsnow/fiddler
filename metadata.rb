a09d4c60bf870155cd841e20b6e71c68e4a3c7eb
Linus Ruth <linus.ruth@changepoint.com>
2018-05-02 12:36:53 -0700

    feat: normalize scaffolding across all windowschefcookbooks repos

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,10 @@ maintainer       'Shawn Neal'
 maintainer_email 'sneal@sneal.net'
 source_url       'https://github.com/windowschefcookbooks/fiddler'
 issues_url       'https://github.com/windowschefcookbooks/fiddler/issues'
+chef_version     '>= 12.6' if respond_to?(:chef_version)
 license          'Apache 2.0'
 description      'Installs/Configures Fiddler'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
+supports         'windows'
+depends          'windows'
